4a0423dd3ada560d4eb77592363998a8c9528e00
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-11-29 07:08:45 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Component
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String message() {
         return "Hello World";
     }
