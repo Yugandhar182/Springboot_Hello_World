03c91554cb718ccf3b4d7f1d33d91bb430bea792
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-11-29 07:15:53 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@ComponentScan(basePackages = "com.example.demo.controllers")
 @SpringBootApplication
 public class DemoApplication {
 
