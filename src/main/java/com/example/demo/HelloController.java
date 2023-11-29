15da8da37a4d26d47cce996835b4701424290795
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-11-29 09:00:59 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,10 +1,8 @@
 package com.example.demo;
-
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Component
+
 @RestController
 public class HelloController {
 
